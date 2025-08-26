efffb5e77b06db64df5416f05965e05484bc2a73
chuma-beep <wisboynelson123@gmail.com>
2025-08-26 18:26:38 +0100

    removed consolelog used for testing

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -23,8 +23,7 @@ export default function AuthPage(){
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle form submission here
-    console.log('Form submitted:', formData);
+    // console.log('Form submitted:', formData);
   };
 
   const toggleAuthMode = () => {
